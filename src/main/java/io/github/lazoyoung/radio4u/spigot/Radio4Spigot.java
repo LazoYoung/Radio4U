89f745ca7282f59ce103979cba4eda74dde1ee42
LazoYoung <myblog11@naver.com>
2018-02-24 01:02:45 +0900

    Project setup

---
+++ src/main/java/io/github/lazoyoung/radio4u/spigot/Radio4Spigot.java
@@ -0,0 +1,12 @@
+package io.github.lazoyoung.radio4u.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Radio4Spigot extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+    
+    }
+    
+}
